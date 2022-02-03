ff0fd56741b2ff157ddf3c86cad83c3febc8655e
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-03 09:27:59 -0800

    06

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-char=c;
-int=i;
-long int=li;
-long long int=lli;
-float=f;  
-printf("Size of a char : %d\n",sizeof(c));
-printf("Size of an int : %d\n", sizeof(i));
-printf("Size of a long int : %d\n", sizeof(li));
-printf("Size of a long long int : %d\n", sizeof(lli));
-printf("Size of a float : %d\n",sizeof(f));
+char c;
+int i;
+long int li;
+long long int lli;
+float f;  
+printf("Size of a char : %lu byte(s)\n",sizeof(c));
+printf("Size of an int : %lu byte(s)\n", sizeof(i));
+printf("Size of a long int : %lu byte(s)\n", sizeof(li));
+printf("Size of a long long int : %lu byte(s)\n", sizeof(lli));
+printf("Size of a float : %lu byte(s)\n",sizeof(f));
 return (0);
 }
