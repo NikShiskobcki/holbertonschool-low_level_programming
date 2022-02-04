e4819077ef8a0f56e73e02584c3a06058bbe230e
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-04 06:26:31 -0800

    prints the alphabet without letters e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 char ch;
- 
- for (ch = 'a' ; ch <= 'z' ; ch++)
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-  if (ch != 'e')
-    {
-      if (ch != 'q')
-	{
+if (ch != 'e')
+{
+if (ch != 'q')
+{
 putchar(ch);
-	}
-    }
- }
- 
+}
+}
+}
+
 
 putchar('\n');
 return (0);
