c0f19217655111c7bf48f8b0e917a97b5e3e9928
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-04 09:59:21 -0800

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,33 +8,32 @@
  */
 int main(void)
 {
-  int i;
-  int n = 1;
-  int j = 2;
-  for (i = '0'; i <= '9'; i++)
-    {
-      for (n = i ; n <= '9'; n++)
-	{
-	  for (j = n ; j <= '9'; j++)
-	    {
-	  if (n != i  && n != j && i !=j)
-	    {
-	      putchar(i);
-	      putchar(n);
-	      putchar(j);
-	      if (i == '7' && n == '8' && j == '9')
-		{
-		  putchar(10);
-		}
-	      else
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	      }
-	    }
-	}
-    }
-
-  return (0);
+int i;
+int n = 1;
+int j = 2;
+for (i = '0'; i <= '9'; i++)
+{
+for (n = i ; n <= '9'; n++)
+{
+for (j = n ; j <= '9'; j++)
+{
+if (n != i  && n != j && i != j)
+{
+putchar(i);
+putchar(n);
+putchar(j);
+if (i == '7' && n == '8' && j == '9')
+{
+putchar(10);
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+return (0);
 }
