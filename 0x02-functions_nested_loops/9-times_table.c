ecd089d8810646bcfcb5a252acd0d361f1041022
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-07 10:25:20 -0800

    09

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,40 +8,23 @@
  */
 void times_table(void)
 {
-int a, b, aux;
-
-for (a = 0; a <= 8; a++)
-{
-_putchar('0');
-_putchar(',');
-if (a == 9)
-{
-break;
-}
-_putchar(' ');
-_putchar(' ');
-}
-
+int a, b, c;
 for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
 {
-aux = a * b;
-if (aux == 0)
+c = a * b;
+if (c < 10)
 {
-_putchar('0');
-}
-else
-if (aux / 10 == 0)
-{
-_putchar(aux + '0');
+_putchar(' ');
+_putchar((c % 10) + '0');
 }
 else
 {
-_putchar(aux / 10  + '0');     
-_putchar(aux % 10 + '0');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 }
-if (aux == a * 9)
+if (b == 9)
 {
 break;
 }
