412d891abf79345360b06ed5a9fc1b5b044f218f
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-07 06:28:48 -0800

    06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @j: input creo
+ * Return: 0 always
+ */
+int _abs(int j)
+{
+if (j <= 0)
+{
+j = j * -1;
+}
+return (j);
+}
