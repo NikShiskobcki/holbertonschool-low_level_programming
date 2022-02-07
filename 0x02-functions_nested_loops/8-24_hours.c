2c647f6cea93da5cba9f59957c4e8966c40d9cce
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-07 08:41:09 -0800

    08

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,40 +2,35 @@
 
 
 /**
- * main - Entry point
+ * jack_bauer - prints 24 hours
  *
  * Return: Always 0 (Success)
  */
 void jack_bauer(void)
 {
-  int hh,mm,aux;
+int hh, mm, aux;
 
-  for (hh=00;hh<=23;hh++)
-  {
-    for (mm=00;mm<=59;mm++)
-      {
-	aux=hh/10;
-	_putchar(aux + '0');
+for (hh = 00; hh <= 23; hh++)
+{
+for (mm = 00; mm <= 59; mm++)
+{
+aux = hh / 10;
+_putchar(aux + '0');
 
-	aux=hh%10;
-	_putchar(aux + '0');
+aux = hh % 10;
+_putchar(aux + '0');
 
-	_putchar(':');
+_putchar(':');
 
-	aux=mm/10;
-	_putchar(aux + '0');
+aux = mm / 10;
+_putchar(aux + '0');
 
-	aux=mm%10;
-	_putchar(aux + '0');
+aux = mm % 10;
+_putchar(aux + '0');
 
-	_putchar('\n');
+_putchar('\n');
 
-
-
-
-      }
-
-  }
 }
 
-
+}
+}
