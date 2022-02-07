aabfb45bf20e6d708e8a29c509f5cfa93d4f3044
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-07 05:33:09 -0800

    03

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c : input creo
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+
+if (c >= 'a')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
