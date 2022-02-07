94d62526813c64053c2cff2d00ff5aac163cb2f9
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-07 11:28:45 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 int i;
 if (n==98)
 {
-_putchar('\n');
+printf("%d", i);
 }
 else
 if (n<98)
@@ -20,19 +20,24 @@ if (n<98)
 for (i=n; i<=98;i++)
 {
 printf("%d", i);
+ if (n != 98)
+   {
 printf("%c", ',');
 printf("%c", ' ');
 	 }
+ }
      }
    else
      {
        for (i=n;i>=98;i--)
 	 {
 	   printf("%d", i);
+	   if (n != 98)
+	     {
 	   printf("%c", ',');
 	   printf("%c",' ');
 	 }
-
+	 }
      }
 
 }
