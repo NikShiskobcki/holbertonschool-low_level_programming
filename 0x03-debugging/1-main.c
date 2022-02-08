7303fb3ab30a66ba330dadd4f683a34c61479b50
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-08 08:41:00 -0800

    01

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int i;
- 
+
 printf("Infinite loop incoming :(\n");
 /*
 *i = 0;
@@ -19,5 +19,6 @@ printf("Infinite loop incoming :(\n");
 *}
 */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
