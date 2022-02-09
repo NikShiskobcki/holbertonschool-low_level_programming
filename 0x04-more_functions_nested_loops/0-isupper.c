384521c4239f8c8df6e75df8ec9e469593850a42
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-09 04:15:34 -0800

    checks for uppercase char

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - checks for uppercase character
+ *
+ * Return: 1 if uppercase, 0 otherwise
+*/
+int _isupper(int c)
+{
+  if (c>='a')
+      return (0);
+  else
+    return(1);
+
+
+
+}
