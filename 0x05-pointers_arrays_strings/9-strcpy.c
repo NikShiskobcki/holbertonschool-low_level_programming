019fe8788bf27f77c5dbae859ecc2fa517430ed8
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-11 11:00:42 -0800

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - prints string and reverses it
+ * @s: input
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i, n, j;
+i = (_strlen(src));
+for (n = 0; n <= i; n++, j++)
+{
+dest[j] = src[n];
+}
+return (dest);
+}
