b689fe6237ebb04bfe1863d74cb1f3f9544c0bbc
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-11 09:52:45 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#inlcude "2-strlen.c"
+
+/**
+ * rev_string - prints string and reverses it
+ * @s: input
+ *
+ */
+void rev_string(char *s)
+{
+int i, n;
+char str;
+i = (_strlen(s) - 1);
+for (n = 0; n <= i; n++, i--)
+{
+str = s[n];
+s[n] = s[i];
+s[i] = str;
+}
+}
