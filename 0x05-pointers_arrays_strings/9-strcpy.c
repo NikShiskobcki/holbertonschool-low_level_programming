d33e4b92abe0f19ff6f78008d43cd683c4519b28
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-11 11:17:40 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,13 +5,15 @@
  * _strcpy - prints string and reverses it
  * @dest: input
  * @src: input
+ * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
 int i, n;
 char str;
-i = (_strlen(src) - 1);
+i = (_strlen(src));
 if (i > 0)
+
 {
 for (n = 0; n <= i; n++)
 {
@@ -19,5 +21,5 @@ str = src[n];
 dest[n] = str;
 }
 }
-return (dest); 
+return (dest);
 }
