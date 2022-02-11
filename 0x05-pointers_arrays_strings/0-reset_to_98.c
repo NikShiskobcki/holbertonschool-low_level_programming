8da86650651ee801f8d4fb57c9f856642240522e
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-11 04:20:47 -0800

    task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value pointed to 98 
+ * reset_to_98 - updates the value pointed to 98
  *
- * 
+ * @n: input;
  */
 void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
