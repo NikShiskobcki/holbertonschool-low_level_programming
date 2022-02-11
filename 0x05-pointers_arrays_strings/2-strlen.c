ecb6258799631e32165702de53fd4b7cef4202f9
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-11 08:18:37 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: input
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+return (i);
+}
