3dec87af511b17a60be20cce75449e4ceaf85b16
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-11 10:16:09 -0800

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,13 +13,13 @@ for (i = 0; str[i] != '\0'; i++)
 }
 if (i % 2 != 0)
 {
-i = ((i - 1) / 2);
+i = ((i / 2) - 1);
 }
 else
 {
 i = i / 2;
 }
-for (j = i + 1; str[j] != '\0'; j++)
+for (j = i; str[j] != '\0'; j++)
 _putchar(str[j]);
 _putchar('\n');
 }
