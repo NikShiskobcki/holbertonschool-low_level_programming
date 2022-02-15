f872eaf54ff7f740eabf34dc82e6a28eac33a798
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-15 09:00:55 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase to upper 
+ * 
+ * 
+ */ 
+char *string_toupper(char * a)
+{
+  int i, x;
+
+  for (i = 0; a[i] != '\0'; i++)
+    {
+      x= a[i];
+      if ((x >= 'a') && (x <= 'z'))
+	{
+      x = x - 32;
+      a[i]=x;
+	}
+    }
+  return (a);
+
+
+}
