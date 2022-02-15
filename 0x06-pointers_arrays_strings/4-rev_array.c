7cf6f86e41817a168cb3a673d8083f45c1894a57
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-15 08:42:23 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array
+ * @a: input
+ *@n: input
+ *
+ */
+void reverse_array(int *a, int n)
+{
+  int arr;
+  int  x, k;
+  x = 0;
+
+  
+
+  for (k = n-1; k > x; k--)
+    {
+      arr = a[x];
+      a[x] = a[k];
+      a[k] = arr;
+      x++;
+
+    }
+    
+  
+
+
+
+}
