5a81c57442d7d6b463ddb58108647a01cba63b64
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-15 10:14:27 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - comp two strings
+ * @ : chars
+ *
+ *
+ * Return: string
+ */
+
+char *leet(char *a)
+{
+  int i, j;
+  char l[] = "aAeEoOtTlL";
+  char n[] = "4433007711";
+
+  for (i = 0; a[i] != '\0'; i++)
+    {
+      for (j = 0; l[j] != '\0'; j++)
+	{
+	  if (a[i] == l[j])
+	    a[i] = n[j];
+	}
+    }
+  return (a);
+}
