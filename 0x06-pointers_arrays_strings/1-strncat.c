e9935d9d9c0715eb7608f847b02db7d421b41d64
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-15 05:25:37 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
   for (i = 0; dest[i] != '\0'; i++)
     {
     }
-  for (k = 0; src[k] != 0; k++)
+  for (k = 0; src[k] != '\0'; k++)
     {
     }
   
