2ca00069717d159fbd5818edd2a942f089715497
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-16 03:56:44 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strncopy - concatenates two strings
- * @dest: input 
+ * _strncpy - concatenates two strings
+ * @dest: input
  * @src : input
  * @n : input
  * Return: dest
@@ -10,10 +10,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-  int k;
-  for (k = 0; ((k < n) && (src[k] != '\0')); k++)
-    dest[k] = src[k];
-  while (k < n)
-    dest[k++] = '\0';
-  return (dest);
+int k;
+for (k = 0; ((k < n) && (src[k] != '\0')); k++)
+dest[k] = src[k];
+while (k < n)
+dest[k++] = '\0';
+return (dest);
 }
