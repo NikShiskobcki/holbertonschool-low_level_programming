4a5904cb60efe869ee531af5f808a274075c9fa4
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-16 04:54:54 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,17 +10,16 @@
 
 char *leet(char *a)
 {
-  int i, j;
-  char l[] = "aAeEoOtTlL";
-  char n[] = "4433007711";
-
-  for (i = 0; a[i] != '\0'; i++)
-    {
-      for (j = 0; l[j] != '\0'; j++)
-	{
-	  if (a[i] == l[j])
-	    a[i] = n[j];
-	}
-    }
-  return (a);
+int i, j;
+char l[] = "aAeEoOtTlL";
+char n[] = "4433007711";
+for (i = 0; a[i] != '\0'; i++)
+{
+for (j = 0; l[j] != '\0'; j++)
+{
+if (a[i] == l[j])
+a[i] = n[j];
+}
+}
+return (a);
 }
