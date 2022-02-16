f4ab07c71c669046292bd2596a9b8e567ae1542c
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-16 04:47:24 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,24 +10,24 @@
 
 char *cap_string(char *a)
 {
-  char aux[] = {'\t', ' ', '\n', ',', ';', '.', '!', '?', '"','(', ')','{','}'};
-  int i, j, max;
+char x[] = {'\t', ' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int i, j, max;
 
-  for (max = 0; a[max] != '\0'; max++)
-    {
-    }
-  max--;
+for (max = 0; a[max] != '\0'; max++)
+{
+}
+max--;
 
-  for (i = 0; i < max; i++)
-    {
-      for (j = 0; aux[j] != '\0'; j++)
-	{
-	  if (a[i] == aux[j])
-	    {
-	      if ((a[i + 1] >= 'a') && (a[i + 1] <= 'z'))
-		a[i + 1] -= 32;
-	    }
-	}
-    }
-  return (a);
+for (i = 0; i < max; i++)
+{
+for (j = 0; x[j] != '\0'; j++)
+{
+if (a[i] == x[j])
+{
+if ((a[i + 1] >= 'a') && (a[i + 1] <= 'z'))
+a[i + 1] -= 32;
+}
+}
+}
+return (a);
 }
