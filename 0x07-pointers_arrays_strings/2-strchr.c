4aa502090ba0d0febc564bc3962f38450bc69ba5
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-21 08:27:19 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _strchr: locates a char in a string
+ * _strchr - locates a char in a string
  * @c: input
+ * @s: input
  * Return: c or NULL
  */
 char *_strchr(char *s, char c)
 {
- 
-  while (*s != c && *s != '\0')
-    s++;
-  if (c != '\0' && *s == '\0')
-    return ('\0');
-  else
-    return (s);
-  
+while (*s != c && *s != '\0')
+s++;
+if (c != '\0' && *s == '\0')
+return ('\0');
+else
+return (s);
+
 }
