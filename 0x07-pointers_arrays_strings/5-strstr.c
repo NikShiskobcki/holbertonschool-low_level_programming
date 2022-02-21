276fe25bc6e5355fd73d78afbcd0a1581e87918b
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-21 11:39:47 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,67 +1,38 @@
 #include "main.h"
-
 /**
- * _strstr - locates substring
- * @haystack: input
- * @needle: input
- * 
- * Return: pointer to begining of substring or null
+ * compare - man
+ * @X : pointer
+ * @Y : pointer
+ * Return: int
+ */
+int compare(const char *X, const char *Y)
+{
+while (*X && *Y)
+{
+if (*X != *Y)
+{
+return (0);
+}
+X++;
+Y++;
+}
+return (*Y == '\0');
+}
+/**
+ * _strstr - man
+ * @needle : pointer
+ * @haystack : pointer
+ * Return: char
  */
 char *_strstr(char *haystack, char *needle)
 {
-  int i, j, flag, x;
-  char *f;
-
-  if (needle[0] == '\0')
-    return (haystack);
-  
-  for (i = 0; haystack[i] != '\0'; i++)
-    {
-      if (haystack[i] != '\0')
-	{
-	  if (haystack[i] == needle[0])
-	    {
-	 
-	      f = &haystack[i];
-	      x = 1;
-	      for (j = i + 1; haystack[j] != '\0'; j++)
-		{
-		  if (needle[x] == '\0')
-		    break;
-		  else
-		    {
-		      if (haystack[j] != needle[x])
-			{
-			  flag = 0;
-			  break;
-			}
-		      else
-			{
-			  flag = 1;
-			  if (needle[x] != '\0')
-			    x++;
-			  
-			}
-		    }
-		}
-	      if (flag == 1)
-		break;
-
-	    }
-	  else
-	    flag = 0;
-       
-	}
-     
-
-      
-      
-      if (flag == 1)
-	break;
-    }
-  if (flag == 0)
-    return ('\0');
-  else
-    return (f);
-
+if (needle[0] == '\0')
+return (haystack);
+while (*haystack != '\0')
+{
+if (((*haystack == *needle) && compare(haystack, needle)))
+return (haystack);
+haystack++;
+}
+return (NULL);
 }
