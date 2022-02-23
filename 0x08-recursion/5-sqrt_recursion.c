d9286ff32b461ae078bad5285c51f7443d2e9a7c
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-23 08:41:34 -0800

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * aux - main
+ * @n : int
+ * @r : int
+ * @rx2 : origin
+ * Return: int
+ */
+
+
+int aux(int n, int r, int rx2)
+{
+if (n == rx2)
+return (r);
+if (n > (rx2 / 2))
+{
+r = r + 1;
+return (aux(n, r, r *r));
+}
+return (-1);
+}
+
+/**
+ * _sqrt_recursion - main
+ * @n : int
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (aux(n, 0, 0));
+}
