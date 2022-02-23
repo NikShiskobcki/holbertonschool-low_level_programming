01676c303c043d00bb4c7b75a750509f3bf8fd73
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-23 09:52:16 -0800

    100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "2-strlen_recursion.c"
+
+/**
+ * aux - main
+ * @size : int
+ * @i : int
+ * @s : char;
+ * Return: int
+ */
+
+int aux(int size, int i, char *s)
+{
+  if ( i >= size / 2)
+    return (1);
+  if (s[i] == s[size])
+    {
+      return (aux(size -1, i + 1, s));
+    }
+  return (0);
+}
+
+/**
+ * is_polindrome - main
+ * @s : char
+ * Return: int
+ */
+
+int is_palindrome(char *s)
+{
+  int size = _strlen_recursion(s);
+  if (size == 0 || size == 1)
+    return (1);
+  if (size % 2 == 0)
+    size ++;
+  return (aux(size - 1, 0, s));
+
+
+}
