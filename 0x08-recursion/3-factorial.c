3d722c4aa87a20bcd0e7b861f31c97cce3a39830
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-23 05:08:43 -0800

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - prints factorial of number
+ * @n: input
+ * Return: -1 if error, factorial result
+ */
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+
+return (n * factorial(n - 1));
+}
