d48c85efde03e9330540c51c63d5435a7e9ebdc1
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-23 10:24:01 -0800

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,34 +6,34 @@
  * @size : int
  * @i : int
  * @s : char;
+ * @sizecpy: size
  * Return: int
  */
 
 int aux(int size, int i, char *s, int sizecpy)
 {
-  if ( i > sizecpy / 2)
-    return (1);
-  if (s[i] == s[size])
-    {
-      return (aux(size -1, i + 1, s, sizecpy));
-    }
-  return (0);
+if (i > sizecpy / 2)
+return (1);
+if (s[i] == s[size])
+{
+return (aux(size - 1, i + 1, s, sizecpy));
+}
+return (0);
 }
 
 /**
- * is_polindrome - main
+ * is_palindrome - main
  * @s : char
  * Return: int
  */
 
 int is_palindrome(char *s)
 {
-  int size = _strlen_recursion(s);
-  int sizecpy;
-  if (size == 0 || size == 1)
-    return (1);
-  sizecpy = size;
-  return (aux(size - 1, 0, s, sizecpy));
-
+int size = _strlen_recursion(s);
+int sizecpy;
+if (size == 0 || size == 1)
+return (1);
+sizecpy = size;
+return (aux(size - 1, 0, s, sizecpy));
 
 }
