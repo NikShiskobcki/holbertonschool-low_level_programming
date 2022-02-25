deb62c0500aa63755b98dc965c3de6ec0a3f9f88
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-25 08:23:48 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,20 +2,17 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: 0
  */
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-  int n = -1;
-    while(argc--)
-      n++;
-    printf("%d\n", n);
-    
-    return (0);
-      
-  
+int n = -1;
+while (argc--)
+n++;
+printf("%d\n", n);
 
-
+return (0);
 }
