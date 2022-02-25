30bb8485e7c62b13c216b953ef1b5a0fc7e3f137
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-25 08:18:33 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * main - entry point
+ * @argc: input
+ * @argv : input
  */
 int main(int argc, char *argv[])
 {
-  while(argc--)
-  printf("%s\n", *argv++);
+while(argc--)
+printf("%s\n", *argv++);
 
-  return(0);
+return(0);
 }
