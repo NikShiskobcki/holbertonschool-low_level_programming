d28971ae0c3eff6d334e9a13549213c7bba74d0f
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-25 04:10:28 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+  int n = -1;
+  int res, n1, n2;
+  while(argc--)
+    n++;
+
+  if (n < 2)
+    {
+    printf("Error\n");
+    return (0);
+    }
+  
+ n1 = atoi(argv[1]);
+ n2 = atoi(argv[2]);
+ res = n1 * n2;
+
+ printf("%d\n", res);
+  
+
+  return(0);
+}
