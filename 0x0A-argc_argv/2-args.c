79038349d86bb7bb5a1509427b54d4b7cb9026b6
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-25 08:27:06 -0800

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - entry point
+ * @argc: entry point
+ * @argv: entry point
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
-  while(argc--)
-    printf("%s\n", *argv++);
-  exit(EXIT_SUCCESS);
+while (argc--)
+printf("%s\n", *argv++);
+return (0);
 }
