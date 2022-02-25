a93e7f628f6c1b53a0428c072bf47264dfb63fca
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-25 08:31:37 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,29 +3,29 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ * main - entry point
+ * @argc: entry point
+ * @argv: entry point
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
-  int n = -1;
-  int res, n1, n2;
-  while(argc--)
-    n++;
+int n = -1;
+int res, n1, n2;
+while (argc--)
+n++;
 
-  if (n < 2)
-    {
-    printf("Error\n");
-    return (0);
-    }
-  
- n1 = atoi(argv[1]);
- n2 = atoi(argv[2]);
- res = n1 * n2;
+if (n < 2)
+{
+printf("Error\n");
+return (0);
+}
 
- printf("%d\n", res);
-  
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+res = n1 *n2;
 
-  return(0);
+printf("%d\n", res);
+
+return (0);
 }
