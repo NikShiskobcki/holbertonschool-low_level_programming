d96a64da587ea774586fb566cc2cdb6a60095f3b
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-25 03:50:52 -0800

    task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+  int n = -1;
+    while(argc--)
+      n++;
+    printf("%d\n", n);
+    
+    return (0);
+      
+  
+
+
+}
