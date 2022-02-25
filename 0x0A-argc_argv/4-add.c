1a340a29c9eb946a2cca585555efbf03d41779ce
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-02-25 08:34:18 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,38 +4,38 @@
 #include <string.h>
 
 /**
- *
- *
- *
+ * main - entry point
+ * @argc: entry point
+ * @argv: entry point
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-  int x, y;
-  int sum = 0;
+int x, y;
+int sum = 0;
 
-  if (argc == 1)
-    {
-      printf("0\n");
-      return (0);
-    }
-  
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
 
-  for (x = 1; x < argc; x++)
-    {
-      for (y = 0; y < (int)strlen(argv[x]); y++)
-	{
-	  if (argv[x][y] >= 48 && argv[x][y] <= 57)
-	    {
-	    }
-	  else
-	    {
-	      printf("Error\n");
-	      return (1);
-	    }
-	}
-      sum = sum + atoi(argv[x]);
-    }
-  printf("%d\n", sum);
 
-  return (0);
+for (x = 1; x < argc; x++)
+{
+for (y = 0; y < (int)strlen(argv[x]); y++)
+{
+if (argv[x][y] >= 48 && argv[x][y] <= 57)
+{
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+sum = sum + atoi(argv[x]);
+}
+printf("%d\n", sum);
+return (0);
 }
