c10cd58fcc690dc06657502af75a9509992d1113
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-02 05:27:23 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,28 +8,26 @@
  */
 char *_strdup(char *str)
 {
-  int i, len, j;
-  char *newstr;
-  if (str == NULL)
-    return (NULL);
+int i, len, j;
+char *newstr;
+if (str == NULL)
+return (NULL);
 
-  for (i = 0; str[i] != '\0'; i++)
-    {
-    }
-  
-  len = i;
-  newstr = malloc(len * sizeof(char));
+for (i = 0; str[i] != '\0'; i++)
+{
+}
 
-  if (newstr ==  NULL)
-    return (NULL);
+len = i + 1;
+newstr = malloc(len * sizeof(char));
+
+if (newstr ==  NULL)
+return (NULL);
 
 
-  for (j = 0; j <= len; j++)
-    {
-      newstr[j] = str[j];
-    }
-  
-  
-  return (newstr);
+for (j = 0; j <= len; j++)
+{
+newstr[j] = str[j];
+}
 
+return (newstr);
 }
