d1ce7846119a1bcd9610aa3b672130db3127111e
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-02 09:10:34 -0800

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,37 +4,38 @@
  * alloc_grid - main
  * @width: input
  * @height: input
+ * Return: grid
  */
 int **alloc_grid(int width, int height)
 {
-  int w, i;
-  int **res;
+int w, i;
+int **res;
 
-  if ((width <= 0) || (height <= 0))
-    return (NULL);
+if ((width <= 0) || (height <= 0))
+return (NULL);
 
-  res = malloc(height * sizeof(int *));
+res = malloc(height * sizeof(int *));
 
-  if (res == NULL)
-    return (NULL);
+if (res == NULL)
+return (NULL);
 
-  for (i = 0; i < height; i++)
-    {
-      res[i] = malloc(width * sizeof(int));
-      if (res[i] == NULL)
-	{
-	  for (i = i - 1; i >= 0; i--)
-	  free(res[i]);
-	  free(res);
-	  return (NULL);
-	}
-      else
-	{
-	  for (w = 0; w < width; w++)
-	    {
-	      res[i][w] = 0;
-	    }
-	}
-    }
-  return (res);
+for (i = 0; i < height; i++)
+{
+res[i] = malloc(width * sizeof(int));
+if (res[i] == NULL)
+{
+for (i = i - 1; i >= 0; i--)
+free(res[i]);
+free(res);
+return (NULL);
+}
+else
+{
+for (w = 0; w < width; w++)
+{
+res[i][w] = 0;
+}
+}
+}
+return (res);
 }
