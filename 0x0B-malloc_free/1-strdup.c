c028a4a5eae1baefe07b5a797b288ec9ec2a73e0
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-02 05:25:17 -0800

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - main
+ * @str: input
+ * Return: char
+ *
+ */
+char *_strdup(char *str)
+{
+  int i, len, j;
+  char *newstr;
+  if (str == NULL)
+    return (NULL);
+
+  for (i = 0; str[i] != '\0'; i++)
+    {
+    }
+  
+  len = i;
+  newstr = malloc(len * sizeof(char));
+
+  if (newstr ==  NULL)
+    return (NULL);
+
+
+  for (j = 0; j <= len; j++)
+    {
+      newstr[j] = str[j];
+    }
+  
+  
+  return (newstr);
+
+}
