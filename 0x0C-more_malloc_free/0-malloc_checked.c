3318a1310fcde736287cbe4f984f6044f7c4460f
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-03 07:11:29 -0800

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,15 @@
 #include "main.h"
 #include <limits.h>
 /**
- *
- *
- *
+ * malloc_checked - main
+ * @b: input
+ * Return: mal
  */
 void *malloc_checked(unsigned int b)
 {
-  
-  void *mal = malloc(b);
-  if (mal == NULL)
-    exit (98);
-  return(mal);
-   
-  
+void *mal = malloc(b);
+
+if (mal == NULL)
+exit(98);
+return (mal);
 }
