8d8a24867087372b81bf9f9648ee0b7a361071f4
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-03 08:31:53 -0800

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * array_range - entry
+ * @min: input
+ * @max: input
+ * Return: mal
  */
 int *array_range(int min, int max)
 {
-  int i, x, largo;
-  int *mal;
-  if (min > max)
-    return(NULL);
-  if (min == max)
-    {
-      mal = malloc(1 * sizeof(int));
-      if (mal == NULL)
-	return (NULL);
-      mal[0] = min;
-      return (mal);
-    }
-  largo = (max - min);
-  mal = malloc((largo + 1) * sizeof(int));
-  if (mal == NULL)
-    return (NULL);
-  x = 0; 
-  for (i = min; i <= max; i++)
-    {
-      mal[x] = i;
-      if (x < largo)
-      x++;
-    }
-  return (mal);
+int i, x, largo;
+int *mal;
+
+if (min > max)
+return (NULL);
+
+if (min == max)
+{
+mal = malloc(1 * sizeof(int));
+if (mal == NULL)
+return (NULL);
+mal[0] = min;
+return (mal);
+}
+
+largo = (max - min);
+mal = malloc((largo + 1) * sizeof(int));
+if (mal == NULL)
+return (NULL);
+x = 0;
+for (i = min; i <= max; i++)
+{
+mal[x] = i;
+if (x < largo)
+x++;
+}
+return (mal);
 }
