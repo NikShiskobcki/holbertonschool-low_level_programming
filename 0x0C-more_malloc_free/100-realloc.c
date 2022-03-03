c4a8c2846c985093591d1740025c05c7364d6592
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-03 09:59:06 -0800

    fkej

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,8 +21,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
   
   if (ptr == NULL)
     {
-    malloc(new_size);
+    ptr = malloc(new_size);
     return(ptr);
     }
     return(ptr);
+
+    
  }
