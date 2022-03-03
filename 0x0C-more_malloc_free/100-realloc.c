9db5f740e0754856a5a2ae6d932c34ce0cbb09a8
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-03 10:09:17 -0800

    fkej

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,30 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * _realloc - main
- * @ptr: input
- * @old_size: input
- * @new_size: input
- * Return: void
+ * @ptr : int
+ * @old_size : int
+ * @new_size : int
+ * Return: array
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+void *ptrNew;
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+return (ptr);
+}
+if ((ptr != NULL) && (new_size == 0))
+{
+free(ptr);
+return (NULL);
+}
+if (old_size >= new_size)
+return (ptr);
+ptrNew = malloc(new_size);
 
-  if ((new_size == 0) && (ptr != NULL))
-    {
-    free(ptr);
-    return (NULL);
-    }
+if (ptrNew == NULL)
+return (NULL);
 
-  if (new_size <= old_size)
-    return (ptr);
-  
-  if (ptr == NULL)
-    {
-    ptr = malloc(new_size);
-    return(ptr);
-    }
-    return(ptr);
-
-    
- }
+memcpy(ptrNew, ptr, old_size);
+free(ptr);
+return (ptrNew);
+}
