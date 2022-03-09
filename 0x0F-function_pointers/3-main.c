b48b26dc6e5ce02f3f2faeee2c9148d99ebd127b
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-09 09:43:46 -0800

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * main - entry
+ * @argc: input
+ * @argv: input
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
@@ -13,20 +14,17 @@ if (argc != 4)
 {
 printf("Error\n");
 exit(98);
-    }
-  
-  a = atoi(argv[1]);
-  b = atoi(argv[3]);
+}
 
-  if (get_op_func(argv[2]) == NULL)
-    {
-      printf("Error");
-      exit(99);
-    }
- 
-  printf("%d\n", get_op_func(argv[2])(a, b));
-  return(0);
+a = atoi(argv[1]);
+b = atoi(argv[3]);
 
+if (get_op_func(argv[2]) == NULL)
+{
+printf("Error");
+exit(99);
+}
 
-
+printf("%d\n", get_op_func(argv[2])(a, b));
+return (0);
 }
