ba5ca8cc91a83f9116670ab75b36094b80cf8e67
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-09 09:31:55 -0800

    main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc != 4)
+{
+printf("Error");
+exit(98);
+    }
+  
+  a = atoi(argv[1]);
+  b = atoi(argv[3]);
+
+  if (get_op_func(argv[2]) == NULL)
+    {
+      printf("Error");
+      exit(99);
+    }
+ 
+  printf("%d\n", get_op_func(argv[2])(a, b));
+  return(0);
+
+
+
+}
