059c11819c5badc6396c11759d648260d72eef56
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-09 08:23:48 -0800

    functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds
+ * @a: input
+ * @b: input
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - substracts
+ * @a: input
+ * @b: input
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiplies
+ * @a: input
+ * @b: input
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divides
+ * @a: input
+ * @b: input
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - remainder
+ * @a: input
+ * @b: input
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
