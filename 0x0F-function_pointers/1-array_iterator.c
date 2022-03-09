5e2670fc135249ea2e2c680f239a6619520f39ce
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-09 07:45:15 -0800

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned long int i;
 if ((size > 0) && (action != NULL))
 {
+if (array != NULL)
+{
 for (i = 0; i < size; i++)
 {
 action(array[i]);
+}
 }
 }
 }
