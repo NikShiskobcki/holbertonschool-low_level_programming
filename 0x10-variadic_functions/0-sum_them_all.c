83431082a58e677b4247967bfdad7d3da8a1e6f6
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-10 05:34:04 -0800

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all args
+ * @n: input
+ * Return: 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+int res;
+va_list a;
+unsigned int i;
+
+res = 0;
+va_start(a, n);
+
+for (i = 0; i < n; i++)
+{
+res += va_arg(a, int);
+}
+va_end(a);
+
+return (res);
+}
