2c5585dce28d9dc4b60bf4bfcecc9ea866f70ebf
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-09 18:21:19 -0800

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,29 +2,37 @@
 #include <stdlib.h>
 
 /**
- * main - prints the opcodes of his function
- * @argc: int
- * @argv: chars
- * Return: O
+ * main - prints opcodes
+ * @argc: num of arg
+ * @argv: array of args
+ *
+ * Return: 0 on Success
  */
-
 int main(int argc, char *argv[])
 {
+  int b, i;
+  char *array;
+
   if (argc != 2)
     {
       printf("Error\n");
       exit(1);
     }
-
-  if (atoi(argv[1]) < 0)
+  b = atoi(argv[1]);
+  if (b < 0)
     {
       printf("Error\n");
       exit(2);
     }
-  system("gcc 100-main_opcodes.c -c");
-  system("objdump -D 100-main_opcodes.o");
- 
-    
-	
+  array = (char *)main;
+  for (i = 0; i < b; i++)
+    {
+      if (i == b - 1)
+	{
+	  printf("%02hhx\n", array[i]);
+	  break;
+	}
+      printf("%02hhx ", array[i]);
+    }
   return (0);
 }
