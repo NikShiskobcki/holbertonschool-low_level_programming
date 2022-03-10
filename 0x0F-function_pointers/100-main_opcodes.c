aa4661c53b284a0bc989ddba2d0bae5421df49c7
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-09 18:33:46 -0800

    task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,34 +5,38 @@
  * main - prints opcodes
  * @argc: num of arg
  * @argv: array of args
- *
  * Return: 0 on Success
  */
 int main(int argc, char *argv[])
 {
-  int i, j;
-char *array;
+int i, j;
+char *arr;
 
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
+
 j = atoi(argv[1]);
+
 if (j < 0)
 {
 printf("Error\n");
 exit(2);
 }
-array = (char *)main;
+
+arr = (char *)main;
+
 for (i = 0; i < j; i++)
 {
 if (i == j - 1)
 {
-printf("%02hhx\n", array[i]);
+printf("%02hhx\n", arr[i]);
 break;
 }
-printf("%02hhx ", array[i]);
+printf("%02hhx ", arr[i]);
 }
 return (0);
 }
+/* %02hhx turns byte into two hexadecimal digits */
