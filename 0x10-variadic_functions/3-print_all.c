8e8c8ccd97602eb380f4105b68b822fcc8bbc469
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-10 08:29:52 -0800

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
  */
 void print_c(va_list c)
 {
-  printf("%c", va_arg(c, int));
+printf("%c", va_arg(c, int));
 }
 
 /**
@@ -16,7 +16,7 @@ void print_c(va_list c)
  */
 void print_i(va_list i)
 {
-  printf("%d", va_arg(i, int));
+printf("%d", va_arg(i, int));
 }
 
 
@@ -26,7 +26,7 @@ void print_i(va_list i)
  */
 void print_f(va_list f)
 {
-  printf("%f", va_arg(f, double));
+printf("%f", va_arg(f, double));
 }
 
 
@@ -36,55 +36,48 @@ void print_f(va_list f)
  */
 void print_s(va_list s)
 {
-  char *str = va_arg(s, char *);
-  if (str == NULL)
-  str = "(nil)";
-  printf("%s", str);
+char *str = va_arg(s, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
 }
 
 
 
 /**
- *
- *
- *
+ * print_all - prints all
+ * @format: input
  *
  */
 void print_all(const char * const format, ...)
 {
-  format_t x[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{NULL, NULL}
-                };
-  int i = 0, j;
-  va_list a;
-  char *sep = "";
-  va_start(a, format);
-  while (format && format[i])
-    {
-      j = 0;
-      while (x[j].form != NULL)
-	{
-	  if (*(x[j].form) == format[i])
-	    {
-	      printf("%s", sep);
-	      x[j].f(a);
-	      sep = ", ";
-	      break;
-	    }
-	  j++;
-
-	}
-      i++;
-
-
-
-
-
-    }
-  va_end(a);
-  printf("\n");
+format_t x[] = {
+{"c", print_c}
+{"i", print_i},
+{"f", print_f},
+{"s", print_s},
+{NULL, NULL}
+};
+int i = 0, j;
+va_list a;
+char *sep = "";
+va_start(a, format);
+while (format && format[i])
+{
+j = 0;
+while (x[j].form != NULL)
+{
+if (*(x[j].form) == format[i])
+{
+printf("%s", sep);
+x[j].f(a);
+sep = ", ";
+break;
+}
+j++;
+}
+i++;
+}
+va_end(a);
+printf("\n");
 }
