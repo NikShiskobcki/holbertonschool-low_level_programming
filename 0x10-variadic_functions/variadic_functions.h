c6d7857cb89f9990bbc797b46f337d65ae6b5edb
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-10 08:25:24 -0800

    main

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,24 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * format - Struct format
+ *
+ * @form: The operator
+ * @f: The function associated
+ */
+typedef struct format
+{
+  char *form;
+  void (*f)(va_list);
+} format_t;
+
+void print_c(va_list c);
+void print_i(va_list i);
+void print_f(va_list f);
+void print_s(va_list s);
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
