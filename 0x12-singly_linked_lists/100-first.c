204734c195aceaaba0c59323bf73839c41ae8996
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-17 09:45:54 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_before_main - prints before main
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
