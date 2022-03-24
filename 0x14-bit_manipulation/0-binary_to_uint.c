5fcfc98e9e2bc244f5943b22873585ce809d86c3
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-24 08:51:44 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int x = 0;
+int i = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		x <<= 1;
+		x += b[i] - '0';
+		i++;
+	
+	}
+	
+	return (x);
+
+}
