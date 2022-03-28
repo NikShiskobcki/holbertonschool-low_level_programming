5a5c308bf567222e11a6f74f4d6d68a1de6a1196
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-28 14:45:30 -0700

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main
+ * @argc: input
+ * @argv: input
+ */
+void main(int argc, char *argv[])
+{
+
+		if (argc != 3)
+		{
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+			exit(97);
+		}
+
+
+
+
+}
