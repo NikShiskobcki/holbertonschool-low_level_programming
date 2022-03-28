794425078b9054cf36e32559c2a0946fc57160af
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-28 14:51:53 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,20 +1,35 @@
 #include "main.h"
 
 /**
+ *
+ *
+ *
+ */
+int cpy(char *file_from, char *file_to)
+{
+if (file_from && file_to)
+	return (0);
+return (0);
+
+}
+
+
+
+/**
  * main - main
  * @argc: input
  * @argv: input
  */
-void main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 
 		if (argc != 3)
 		{
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 			exit(97);
 		}
 
+cpy(argv[1], argv[2]);
 
-
-
+return (0);
 }
