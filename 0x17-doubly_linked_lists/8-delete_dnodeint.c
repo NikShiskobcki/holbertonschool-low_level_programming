0fef7f491a1d4fedfdcbf0ad89f8c385a87e051b
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-05 10:28:40 -0700

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+unsigned int i = 0;
+dlistint_t *node = *head;
+dlistint_t *r;
+
+	if (node == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		r = *head;
+		*head = r->next;
+		free(r);
+		return (1);
+	}
+	while (i != (index - 1) && node)
+	{
+		node = node->next;
+		i++;
+	}
+	if (node && (i == (index - 1)))
+	{
+		r = node->next;
+		node->next = node->next->next;
+		if (node->next->next != NULL)
+			node->next->next->prev = node->next;
+		free(r);
+		return (1);
+	}
+	return (-1);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -25,6 +25,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 void free_dlistint(dlistint_t *head);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 
 
