bea0c0a0d38bfe38a8911b4db25a7258c57c8a38
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-05 07:59:13 -0700

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- *
- *
+ *add_dnodeint - adds node to beginning of list
+ * @head: input
+ * @n: input
+ * Return: new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
