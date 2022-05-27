4b1042a8512bab12f628b1a818874bc6ff48ed26
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-27 08:11:20 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashT = NULL;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	hashT = malloc(sizeof(hash_node_t *) * size);
+	if hashT = NULL
+		return (NULL);
+
+	hashT->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashT->array == NULL)
+	{
+		free(hashT);
+		return (NULL);
+	}
+	hashT->size = size;
+	for (i = 0, i < size, i++)
+		hashT->array[i] = NULL;
+	return (hashT)
+}
