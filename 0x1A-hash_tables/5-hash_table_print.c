81a62bf793701d6280252d3d88c8d176d7842dc9
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-27 09:03:29 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - prints hash table
+ *@ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *p = NULL;
+	char *n = NULL;
+	
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	n = "";
+	for (i = 0; i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p)
+		{
+			printf("%s'%s': '%s'", n, p->key, p->value);
+			n = ", ";
+			p = p->next;
+		}
+
+	}
+	printf("}\n");
+}
